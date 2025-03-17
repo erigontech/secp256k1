147ce0b48b9552e8d76542f05204c8279fbf9379
Andrew Ashikhmin <34320705+yperbasis@users.noreply.github.com>
2025-03-17 16:31:59 +0100

    Update bitcoin-core/secp256k1 to v0.6.0 (#5)
    * libsecp256k1 v0.6.0
    
    * Fix compilation
    
    * fix linkage
    
    * restore ext functions
    
    * restore comment
    
    * remove unused import
    
    * correct secp256k1_fe_set_b32 version
    
    * defaults from CMakeLists.txt

--- ext.h
+++ ext.h
@@ -109,14 +109,14 @@ int secp256k1_ext_scalar_mul(const secp256k1_context* ctx, unsigned char *point,
 	ARG_CHECK(scalar != NULL);
 	(void)ctx;
 
-	secp256k1_fe_set_b32(&feX, point);
-	secp256k1_fe_set_b32(&feY, point+32);
+	secp256k1_fe_set_b32_mod(&feX, point);
+	secp256k1_fe_set_b32_mod(&feY, point+32);
 	secp256k1_ge_set_xy(&ge, &feX, &feY);
 	secp256k1_scalar_set_b32(&s, scalar, &overflow);
 	if (overflow || secp256k1_scalar_is_zero(&s)) {
 		ret = 0;
 	} else {
-		secp256k1_ecmult_const(&res, &ge, &s, 256);
+		secp256k1_ecmult_const(&res, &ge, &s);
 		secp256k1_ge_set_gej(&ge, &res);
 		/* Note: can't use secp256k1_pubkey_save here because it is not constant time. */
 		secp256k1_fe_normalize(&ge.x);
--- secp256.go
+++ secp256.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
 
-// +build !gofuzz cgo
+//go:build !gofuzz && cgo
 
 // Package secp256k1 wraps the bitcoin secp256k1 C library.
 package secp256k1
@@ -11,33 +11,17 @@ package secp256k1
 #cgo CFLAGS: -I./libsecp256k1
 #cgo CFLAGS: -I./libsecp256k1/src/
 
-#ifdef __SIZEOF_INT128__
-#  define HAVE___INT128
-#  define USE_FIELD_5X52
-#  define USE_SCALAR_4X64
-#else
-#  define USE_FIELD_10X26
-#  define USE_SCALAR_8X32
-#endif
-
-#define USE_ENDOMORPHISM
-#define USE_NUM_NONE
-#define USE_FIELD_INV_BUILTIN
-#define USE_SCALAR_INV_BUILTIN
 #if defined(__x86_64__)
-#define USE_FIELD_5X52
-#define USE_SCALAR_4X64
-#define HAVE___INT128
-#else
-#define USE_FIELD_10X26
-#define USE_SCALAR_8X32
+#define USE_ASM_X86_64 1
 #endif
 #define ECMULT_WINDOW_SIZE 15
-#define ECMULT_GEN_PREC_BITS 4
-#define USE_ENDOMORPHISM
+#define COMB_BLOCKS 43
+#define COMB_TEETH 6
 #define NDEBUG
+#define ENABLE_MODULE_RECOVERY 1
 #include "./libsecp256k1/src/secp256k1.c"
-#include "./libsecp256k1/src/modules/recovery/main_impl.h"
+#include "./libsecp256k1/src/precomputed_ecmult.c"
+#include "./libsecp256k1/src/precomputed_ecmult_gen.c"
 #include "ext.h"
 
 typedef void (*callbackFunc) (const char* msg, void* data);
